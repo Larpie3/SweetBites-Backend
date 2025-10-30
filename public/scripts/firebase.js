a4754c33e79f7719f39700f830092436649e5a71
Ralph Castañares <ralphcstnrs3@gmail.com>
2025-10-30 16:29:13 +0800

    Update firebase.js

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -1,15 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCkxYnwFBOTO_vz6bkJJWM1tSatq4H6yeY",
   authDomain: "sweetbites-admin-console.firebaseapp.com",
   projectId: "sweetbites-admin-console",
+  storageBucket: "sweetbites-admin-console.firebasestorage.app",
+  messagingSenderId: "125142981711",
+  appId: "1:125142981711:web:7ad785732b705597069e3a"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-console.log("✅ Firebase initialized");
+console.log("✅ Firebase fully initialized (Auth, Firestore, Storage)");
+
